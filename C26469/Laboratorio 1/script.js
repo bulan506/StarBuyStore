9b484669993de2fb03ec595a1ea2e5d1750d952d
ENRD10 <enlrd10@gmail.com>
2024-03-20 05:12:06 -0600

    Tarea 2 Carrusel

---
+++ C26469/Laboratorio 1/script.js
@@ -0,0 +1,23 @@
+if (window.matchMedia("(min-width: 576px)").matches){
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+
+  $(".carousel-control-next").on("click", function () {
+    if (scrollPosition < (carouselWidth - cardWidth * 4)) {
+      scrollPosition += cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600);
+    }
+  });
+
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $(".carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+}
+
