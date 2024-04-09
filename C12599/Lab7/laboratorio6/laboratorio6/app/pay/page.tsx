9a5fe48ffaeec3c7bd30c954d529c85255d58822
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-04-08 23:07:34 -0600

    Update page.tsx

--- C12599/Lab7/laboratorio6/laboratorio6/app/pay/page.tsx
+++ C12599/Lab7/laboratorio6/laboratorio6/app/pay/page.tsx
@@ -17,7 +17,6 @@ const PayPage: React.FC = () => {
     });
 
     useEffect(() => {
-        // Código para cargar el carrito desde localStorage al inicio
         let cartDataString = localStorage.getItem('cartData');
         if (cartDataString !== null) {
             let cartData = JSON.parse(cartDataString);
@@ -60,62 +59,53 @@ const PayPage: React.FC = () => {
 
     const updateLocalStorage = (updatedCart: any) => {
         localStorage.setItem('cartData', JSON.stringify(updatedCart));
-    };
-    const handleSubmit = async () => {
-        const { direccionEntrega, metodoPago, comprobante, productos } = cart;
+    };const handleSubmit = async () => {
+    const { direccionEntrega, metodoPago, comprobante, productos } = cart;
+    const esPedidoValido = direccionEntrega && metodoPago && (metodoPago === 'efectivo' || metodoPago === 'sinpe');
+    
+    if (esPedidoValido) {
+        const productIds = productos.map((producto: any) => String(producto.id));
 
-        if (direccionEntrega && metodoPago && (metodoPago === 'efectivo' || metodoPago === 'sinpe')) {
-            const productIds = productos.map((producto: any) => String(producto.id));
-
-            let paymentMethodValue = 0;
-            if (metodoPago === 'sinpe') {
-                paymentMethodValue = 1;
-            }
-
-            const dataToSend = {
-                productIds: productIds,
-                address: direccionEntrega,
-                paymentMethod: paymentMethodValue
-            };
-
-            console.log('Data a enviar:', dataToSend);
-
-            try {
-                const response = await fetch('https://localhost:7043/api/Cart', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(dataToSend)
-                });
-
-                if (response.ok) {
-                    console.log('datos enviados correctamente')
-                    setCart(prevCart => ({ ...prevCart, confirmacion: '¡Pago confirmado correctamente!' }));
-                    localStorage.removeItem('cartData');
-                    setCart({
-                        productos: [],
-                        direccionEntrega: '',
-                        metodoPago: '',
-                        comprobante: '',
-                        confirmacion: '',
-                        numeroPago: 0,
-                        numeroCompra: Math.floor(Math.random() * 100000000),
-                        isCartEmpty: true
-                    });
-                } else {
-                    const errorResponseData = await response.json();
-                    throw new Error(errorResponseData.message || 'Error al procesar el pago');
-                }
-            } catch (error) {
-                console.error('Error al enviar datos de pago:', error);
-                setCart(prevCart => ({ ...prevCart, confirmacion: 'Error al procesar el pago. Inténtalo de nuevo más tarde.' }));
-            }
-        } else {
-            setCart(prevCart => ({ ...prevCart, confirmacion: 'Por favor, complete todos los campos requeridos o agregue ítems al carrito.' }));
+        let paymentMethodValue = 0;
+        if (metodoPago === 'sinpe') {
+            paymentMethodValue = 1;
         }
-    };
 
+        const dataToSend = {
+            productIds: productIds,
+            address: direccionEntrega,
+            paymentMethod: paymentMethodValue
+        };
+
+        const response = await fetch('https://localhost:7043/api/Cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend)
+        });
+
+        if (response.ok) {
+            setCart(prevCart => ({ ...prevCart, confirmacion: '¡Pago confirmado correctamente!' }));
+            localStorage.removeItem('cartData');
+            setCart({
+                productos: [],
+                direccionEntrega: '',
+                metodoPago: '',
+                comprobante: '',
+                confirmacion: '',
+                numeroPago: 0,
+                numeroCompra: Math.floor(Math.random() * 100000000),
+                isCartEmpty: true
+            });
+        } else {
+            const errorResponseData = await response.json();
+            throw new Error('Error al enviar datos de pago: ' + JSON.stringify(errorResponseData));
+        }
+    } else {
+        setCart(prevCart => ({ ...prevCart, confirmacion: 'Por favor, complete todos los campos requeridos o agregue ítems al carrito.' }));
+    }
+};
     return (
         <div>
             <h1>Pay Page</h1>
@@ -133,7 +123,6 @@ const PayPage: React.FC = () => {
                             onChange={handleDireccionEntregaChange}
                         />
                     </div>
-
                     {cart.direccionEntrega && (
                         <div className="form-group">
                             <label htmlFor="metodoPago">Método de Pago:</label>
@@ -166,13 +155,11 @@ const PayPage: React.FC = () => {
                             </div>
                         </div>
                     )}
-
                     {cart.metodoPago === 'efectivo' && (
                         <div>
                             <p>Número de Compra: {cart.numeroCompra}</p>
                         </div>
                     )}
-
                     <div className='my-4'>
                         <button className="btn btn-primary" onClick={handleSubmit}>
                             Confirmar Pago
