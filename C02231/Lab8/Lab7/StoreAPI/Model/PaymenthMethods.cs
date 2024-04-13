497b2e05ab82a579fcbd629b73d602f8d499ac68
Lani6 <pchavesr29@gmail.com>
2024-04-13 15:55:26 -0600

    PaymenthMethos

--- C02231/Lab8/Lab7/StoreAPI/Model/PaymenthMethods.cs
+++ C02231/Lab8/Lab7/StoreAPI/Model/PaymenthMethods.cs
@@ -1,33 +1,44 @@
 namespace StoreAPI.models;
 
-public abstract class PaymentMethods{
-    public enum Type 
+public abstract class PaymentMethods
+{
+    public enum Type
     {
-    CASH=0,
-    SINPE=1
+        CASH = 0,
+        SINPE = 1
     }
     public Type PaymentType { get; set; }
     public PaymentMethods(PaymentMethods.Type paymentType)
     {
-        PaymentType= paymentType;
+        PaymentType = paymentType;
 
     }
 
     public static PaymentMethods Find(PaymentMethods.Type type)
     {
-        //TODO
-        return null;
+        switch (type)
+        {
+            case Type.CASH:
+                return new Cash();
+            case Type.SINPE:
+                return new Sinpe();
+            default:
+                throw new ArgumentException("Invalid payment type");
+        }
     }
-}
-public sealed class Sinpe:PaymentMethods{
-    public Sinpe(): base(PaymentMethods.Type.SINPE)
-    {
 
+    public sealed class Sinpe : PaymentMethods
+    {
+        public Sinpe() : base(PaymentMethods.Type.SINPE)
+        {
+
+        }
     }
-}
-public sealed class Cash:PaymentMethods{
-    public Cash(): base(PaymentMethods.Type.CASH)
+    public sealed class Cash : PaymentMethods
     {
+        public Cash() : base(PaymentMethods.Type.CASH)
+        {
 
+        }
     }
 }
