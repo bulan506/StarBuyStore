28c6efb0256dc55fbc00cee9515d85a76211c62c
Lani6 <pchavesr29@gmail.com>
2024-04-13 16:03:15 -0600

    Sale

--- C02231/Lab8/Lab7/StoreAPI/Model/Sale.cs
+++ C02231/Lab8/Lab7/StoreAPI/Model/Sale.cs
@@ -8,7 +8,6 @@ public sealed class Sale
     public decimal Amount { get; }
     public PaymentMethods.Type PaymentMethod { get; }
     public string NumberOrder { get; }
-    private static Random random = new Random();
  
 
     public Sale(IEnumerable<Product> products, string address, decimal amount, PaymentMethods.Type paymentMethod, string numberOrder)
@@ -18,20 +17,6 @@ public sealed class Sale
         Amount = amount;
         PaymentMethod = paymentMethod;
         NumberOrder = numberOrder;
-    }
-
-    internal static string GenerateNextPurchaseNumber()
-    {
-        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder numberOrder = new StringBuilder();
-
-        // Agrega 4 caracteres aleatorios
-        for (int i = 0; i < 6; i++)
-        {
-            numberOrder.Append(chars[random.Next(chars.Length)]);
-        }
-
-        return numberOrder.ToString();
     }
 }
 
