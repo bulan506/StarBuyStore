174aa3310601d587a474a907dac10e75817d3dbd
Paula Chaves Rivera <117134960+Lani6@users.noreply.github.com>
2024-04-15 14:46:30 -0600

    Store

--- C02231/Lab8/Lab7/StoreAPI/Model/Store.cs
+++ C02231/Lab8/Lab7/StoreAPI/Model/Store.cs
@@ -168,39 +168,4 @@ public sealed class Store
         Store.Instance = new Store(products, productsCarrusel, 13);
 
     }
-
-
-    public Sale Purchase(Cart cart)
-    {
-        if (cart.ProductIds.Count == 0) throw new ArgumentException("Cart must contain at least one product.");
-        if (string.IsNullOrWhiteSpace(cart.Address)) throw new ArgumentException("Address must be provided.");
-
-        var products = Store.Instance.Products;
-        var taxPercentage = Store.Instance.TaxPercentage;
-
-        // Find matching products based on the product IDs in the cart
-        IEnumerable<Product> matchingProducts = Products.Where(p => cart.ProductIds.Contains(p.Id.ToString())).ToList();
-
-        // Create shadow copies of the matching products
-        IEnumerable<Product> shadowCopyProducts = matchingProducts.Select(p => (Product)p.Clone()).ToList();
-
-        // Calculate purchase amount by multiplying each product's price with the store's tax percentage
-        decimal purchaseAmount = 0;
-        foreach (var product in shadowCopyProducts)
-        {
-            product.Price *= (1 + (decimal)taxPercentage / 100);
-            purchaseAmount += product.Price;
-        }
-
-        PaymentMethods paymentMethod = PaymentMethods.Find(cart.PaymentMethod);
-        PaymentMethods.Type paymentMethodType = paymentMethod.PaymentType;
-
-        // Create a sale object
-        var sale = new Sale(shadowCopyProducts, cart.Address, purchaseAmount, paymentMethodType, Sale.GenerateNextPurchaseNumber());
-
-
-
-        return sale;
-
-    }
 }
