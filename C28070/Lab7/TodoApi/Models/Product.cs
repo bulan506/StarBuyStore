de7c216a71c9d1f276875de7197d6c62305ffd38
varela13dani <maria.varelacordero@ucr.ac.cr>
2024-04-19 23:44:47 -0600

    Lab9C28070

--- C28070/Lab7/TodoApi/Models/Product.cs
+++ C28070/Lab7/TodoApi/Models/Product.cs
@@ -7,10 +7,7 @@ public class Product : ICloneable
     public decimal price { get; set; }
     public required string description { get; set; }
 
-  //  public Guid Uuid { get; set; }
-
-        // Implementation of the ICloneable interface, cada vez que llamo a clone se crea instancia nueva.
-    public object Clone() 
+      public object Clone() 
     {
         return new Product
         {
