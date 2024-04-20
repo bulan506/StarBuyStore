63fbc4bb1f0630cfa89ce2629070807c4bb8f4e9
GaCha22 <gchavesrojas@gmail.com>
2024-04-20 11:07:44 -0600

    Request changes resolved

--- C22202/Lab7/Core/DB/ProductDB.cs
+++ C22202/Lab7/Core/DB/ProductDB.cs
@@ -46,39 +46,4 @@ public sealed class ProductDB
         }
     }
 
-    /*public List<Product> getProducts(){
-        string connectionString = "Server=localhost;Database=mysql;Uid=root;Pwd=123456;";
-        using (var connection = new MySqlConnection(connectionString))
-        {
-            connection.Open();
-
-            // Begin a transaction
-            using (var transaction = connection.BeginTransaction())
-            {
-                try
-                {
-                    // name VARCHAR(100),
-                    // price DECIMAL(10, 2),
-                    // imgSource VARCHAR(100)
-                    string insertProductQuery = @"SELECT * FROM products";
-
-                    using (var insertCommand = new MySqlCommand(insertProductQuery, connection, transaction))
-                    {
-                        insertCommand.ExecuteNonQuery();
-                    }
-
-
-                    // Commit the transaction if all inserts are successful
-                    transaction.Commit();
-                }
-                catch (Exception)
-                {
-                    // Rollback the transaction if an error occurs
-                    transaction.Rollback();
-                    throw;
-                }
-            }
-        }
-    }*/
-
 }
--- C22202/Lab7/ShopApi/Program.cs
+++ C22202/Lab7/ShopApi/Program.cs
@@ -2,10 +2,10 @@ using ShopApi.db;
 
 var builder = WebApplication.CreateBuilder(args);
 
-StoreDB.CreateMysql();
+
+
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +26,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    StoreDB.CreateMysql();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
