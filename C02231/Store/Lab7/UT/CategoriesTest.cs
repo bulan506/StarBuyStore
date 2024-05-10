bc884e9a1e8fa76ae16858e3f364d109ee1c7701
Lani6 <pchavesr29@gmail.com>
2024-05-09 22:08:46 -0600

    UT Categories

--- C02231/Store/Lab7/UT/CategoriesTest.cs
+++ C02231/Store/Lab7/UT/CategoriesTest.cs
@@ -13,13 +13,83 @@ public class CategoriesTest
     private Categories categories;
     private Store store;
 
+
     [SetUp]
     public void Setup()
     {
         string connectionString = "Server=localhost;Database=store;Port=3306;Uid=root;Pwd=123456;";
         Storage.Init(connectionString);
         saleReportLogic = new SaleReportLogic();
+
+        // Inicializar las instancias necesarias para las pruebas
+        storeDB = new StoreDB();
+        store = Store.Instance; // Aquí obtienes la instancia existente de Store
+        categories = Categories.Instance;
     }
-    //Funcionalidad de filtrar categorias 
+
+    [Test]
+    public void CategoryProducts_ReturnsCorrectProducts()
+    {
+        // Arrange
+        int categoryId = 3; // Categoría de ciencia ficción en este ejemplo
+
+        // Act
+        IEnumerable<Product> products = store.CategoryProducts(categoryId);
+
+        // Assert
+        Assert.IsNotNull(products);
+        Assert.IsTrue(products.Any()); // Verificar que haya al menos un producto
+        Assert.IsTrue(products.All(p => p.ProductCategory.IdCategory == categoryId)); // Verificar que todos los productos sean de la categoría especificada
+    }
+
+    [Test]
+    public void CategoryProducts_ThrowsArgumentException_WhenNegativeCategoryId()
+    {
+        // Arrange
+        int categoryId = -1;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => store.CategoryProducts(categoryId));
+    }
+
+
+
+    [Test]
+    public void GetCategories_ReturnsCorrectCount()
+    {
+        // Arrange
+        int expectedCount = 9; // Suponiendo que hay 9 categorías en la lista
+
+        // Act
+        var _categories = categories.GetCategories();
+
+        // Assert
+        Assert.That(_categories.Count(), Is.EqualTo(expectedCount));
+    }
+
+    [Test]
+    public void GetCategories_ReturnsNotNull()
+    {
+        // Act
+        var _categories = categories.GetCategories();
+
+        // Assert
+        Assert.IsNotNull(_categories);
+        Assert.IsTrue(_categories.Any());
+    }
+
+    [Test]
+    public void GetCategories_ReturnsOrderedCategories()
+    {
+        // Arrange
+        var expectedCategories = new List<string> { "Adventure", "Dystopian", "Fantasy", "Fiction", "Mystery", "NonFiction", "Romance", "Science Fiction", "Young Adult" };
+
+        // Act
+        var _categories = categories.GetCategories();
+
+        // Assert
+        CollectionAssert.AreEqual(expectedCategories, _categories.Select(c => c.Name));
+    }
+
 }
 
