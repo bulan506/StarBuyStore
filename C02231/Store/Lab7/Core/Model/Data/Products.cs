356fc9d9107e7a38a26f6065092261591ff12390
Paula Chaves Rivera <117134960+Lani6@users.noreply.github.com>
2024-05-11 13:55:54 -0600

    Update Products.cs

--- C02231/Store/Lab7/Core/Model/Data/Products.cs
+++ C02231/Store/Lab7/Core/Model/Data/Products.cs
@@ -10,14 +10,14 @@ namespace StoreAPI.models;
 public class Products
 {
     private IEnumerable<Product> allProducts;
-    private Dictionary<int, List<Product>> productsDictionary;
+    private Dictionary<int, List<Product>> productByCategoryId;
     public Products() { }
-    private Products(IEnumerable<Product> allProducts, Dictionary<int, List<Product>> productsDictionary)
+    private Products(IEnumerable<Product> allProducts, Dictionary<int, List<Product>> categoryDictionary)
     {
         if (allProducts == null || allProducts.Count() == 0) throw new ArgumentNullException($"The list of products {nameof(allProducts)}can't be null.");
-        if (productsDictionary == null || productsDictionary.Count() == 0) throw new ArgumentNullException($"The products dictionary {nameof(productsDictionary)}by category can't be null.");
+        if (categoryDictionary == null || categoryDictionary.Count() == 0) throw new ArgumentNullException($"The products dictionary {nameof(categoryDictionary)}by category can't be null.");
         this.allProducts = allProducts;
-        this.productsDictionary = productsDictionary;
+        this.productByCategoryId = categoryDictionary;
     }
     public static Products Instance;
 
@@ -26,36 +26,47 @@ public class Products
     {
 
         var products = Store.LoadProducts();
-        Dictionary<int, List<Product>> productsDictionary = new Dictionary<int, List<Product>>();
+        Dictionary<int, List<Product>> productsByCategoryId = new Dictionary<int, List<Product>>();
         foreach (var product in products)
         {
-            if (!productsDictionary.TryGetValue(product.ProductCategory.IdCategory, out var categoryProducts))
+            if (!productsByCategoryId.TryGetValue(product.ProductCategory.IdCategory, out var categoryProducts))
             {
                 categoryProducts = new List<Product>();
-                productsDictionary[product.ProductCategory.IdCategory] = categoryProducts;
+                productsByCategoryId[product.ProductCategory.IdCategory] = categoryProducts;
             }
             categoryProducts.Add(product);
 
 
         }
 
-        Products.Instance = new Products(products, productsDictionary);
+        Products.Instance = new Products(products, productsByCategoryId);
 
     }
 
 
     public IEnumerable<Product> GetProductsCategory(int categoryId)
     {
-        if (categoryId < 0) throw new ArgumentException($"The {nameof(categoryId)} number must be greater than 0");
+        try
+        {
+            if (categoryId < 0)
+            {
+                throw new ArgumentException($"The {nameof(categoryId)} number must be greater than 0");
+            }
 
-        if (categoryId == 0) return this.allProducts;
+            if (categoryId == 0)
+            {
+                return allProducts;
+            }
 
-        this.productsDictionary.TryGetValue(categoryId, out var products);
-        if (products == null) products = new List<Product>();
-        return products;
+            if (productByCategoryId.TryGetValue(categoryId, out var products))
+            {
+                return products;
+            }
+        }
+        catch (ArgumentException ex)
+        {
+           throw new ArgumentException("The list is empty", ex);
+        }
     }
-
-
-
 
 }
