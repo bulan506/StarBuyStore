58b4db17a35e5a6ec122fb1ca7593e4e0a750e5d
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-05-11 08:14:16 -0600

    Update CategoryTest.cs

--- C12599/lab10/Api/UT/CategoryTest.cs
+++ C12599/lab10/Api/UT/CategoryTest.cs
@@ -8,6 +8,16 @@ namespace UT
     [TestFixture]
     public class CategoriesTests
     {
+
+         private Categories _categories;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Inicializar una instancia de Categories antes de cada test
+            _categories = new Categories();
+        }
+
         [Test]
         public void Categories_ContainExpectedCategories()
         {
@@ -40,5 +50,44 @@ namespace UT
                 Assert.AreEqual(expectedCategories[i].Name, actualCategories[i].Name, $"El Nombre de la categoría {i + 1} no coincide.");
             }
         }
+          [Test]
+        public void GetCategoryById_WithValidId_ReturnsCategory()
+        {
+            // Arrange
+            int validCategoryId = 1; // ID existente en ListCategories
+
+            // Act
+            var result = _categories.GetCategoryById(validCategoryId);
+
+            // Assert
+            Assert.IsNotNull(result); // Verificar que el resultado no sea nulo
+            Assert.AreEqual(validCategoryId, result.Id); // Verificar que el ID del resultado coincida con el ID buscado
+        }
+
+        [Test]
+        public void GetCategoryById_WithInvalidId_ThrowsArgumentException()
+        {
+            // Arrange
+            int invalidCategoryId = 0; // ID inválido
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _categories.GetCategoryById(invalidCategoryId));
+        }
+
+        [Test]
+        public void GetCategoryById_WithExistingId_Returns()
+        {
+            // Arrange
+            int categoryExisting = 1; // ID que existe en ListCategories
+
+            // Act
+            var result = _categories.GetCategoryById(categoryExisting);
+
+            var expectingCategory =     new Category(1, "Electrónica");
+
+            // Assert
+            Assert.AreEqual(result, expectingCategory); // Verificar que el resultado sea igual
+        }
     }
 }
+    
