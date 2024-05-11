fc1461d267d252d20eb491384e7fc88daacce590
Sofia Cordero <marianoda20027@gmail.com>
2024-05-10 23:18:08 -0600

    Categories

---
+++ C12599/lab10/Api/Core/models/Categories.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace storeapi.Models
+{
+    public struct Category
+    {
+        public int Id { get; }
+        public  string Name { get; }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class Categories
+    {
+        public List<Category> ListCategories { get; } = new List<Category>
+        {
+            new Category(1, "Electrónica"),
+            new Category(2, "Moda"),
+            new Category(3, "Hogar y jardín"),
+            new Category(4, "Deportes y actividades al aire libre"),
+            new Category(5, "Belleza y cuidado personal"),
+            new Category(6, "Alimentación y bebidas"),
+            new Category(7, "Libros y entretenimiento"),
+            new Category(8, "Tecnología"),
+            new Category(9, "Deportes")
+        };
+
+        public Categories()
+        {
+            ListCategories.Sort((x, y) => string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return ListCategories.FirstOrDefault(c => c.Id == categoryId);
+        }
+    }
+}
