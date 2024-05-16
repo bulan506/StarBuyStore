077db24b39446681c4e46a2479129c0460b122d1
Gabriel Chaves <69835009+GaCha22@users.noreply.github.com>
2024-05-15 18:30:32 -0600

    Tildes Added

--- C22202/Lab7/Core/Business/CategoriesLogic.cs
+++ C22202/Lab7/Core/Business/CategoriesLogic.cs
@@ -18,14 +18,14 @@ public class CategoriesLogic
             new Category(3, "SSD"),
             new Category(4, "Discos Duros"),
             new Category(5, "CPU"),
-            new Category(6, "Tarjetas Graficas"),
+            new Category(6, "Tarjetas Gráficas"),
             new Category(7, "Teclados"),
             new Category(8, "Memorias RAM"),
             new Category(9, "Headsets"),
             new Category(10, "Fuentes de poder"),
             new Category(11, "Cases"),
             new Category(12, "Tarjetas Madre"),
-            new Category(13, "Microfonos")
+            new Category(13, "Micrófonos")
         };
 
         categoryList.Sort((category1, category2) => string.Compare(category1.name, category2.name));
