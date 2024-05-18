0c62f16796c631b624cc1143036417058f0dcaf2
Alvaro <alvarosiles499@gmail.com>
2024-05-17 18:12:28 -0600

    Codigo Comentado

--- C27507/C27507_Lab7/Core/Models/Products.cs
+++ C27507/C27507_Lab7/Core/Models/Products.cs
@@ -20,19 +20,6 @@ public class Products{
         dictionaryOfProducts = new Dictionary<int, List<Product>>();
         //Llenamos el diccionario
         fillDictionaryOfProducts();
-
-        // Recorremos el diccionario
-        // foreach (var kvp in dictionaryOfProducts)
-        // {
-        //     int categoryId = kvp.Key;
-        //     List<Product> products = kvp.Value;
-
-        //     Console.WriteLine($"Category ID: {categoryId}");
-        //     foreach (var product in products)
-        //     {
-        //         Console.WriteLine($"Product Name: {product.name}");
-        //     }
-        // }
     }
 
     private void fillDictionaryOfProducts(){
